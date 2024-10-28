a593dbec04b3e68d06eca25238896b5f429bd36c
korneevDev <korneev.dev@gmail.com>
2024-10-28 12:35:48 +0300

    initial commit

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+//func getProducts()
+
+func main() {
+	// Создаем новый роутер Gin
+	router := gin.Default()
+
+	// Определяем маршруты
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	fmt.Println("Start server on ", router.BasePath())
+	// Запускаем сервер на порту 8080
+	router.Run(":8080")
+
+}
