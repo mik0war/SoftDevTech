372f07a61b5cf065cc35537667d2d9b5ee928054
mik0war <31614706+mik0war@users.noreply.github.com>
2024-12-09 13:05:18 +0300

    Update Create_database.sql

--- Create_database.sql
+++ Create_database.sql
@@ -1,271 +1,265 @@
-DROP TABLE IF EXISTS SuppliedProduct;
-DROP TABLE IF EXISTS Supply;
-DROP TABLE IF EXISTS Supplier;
-DROP TABLE IF EXISTS WarehouseProduct;
-DROP TABLE IF EXISTS OrderStep;
-DROP TABLE IF EXISTS ProductInOrder;
-DROP TABLE IF EXISTS "Order";
-DROP TABLE IF EXISTS ShoppingCartProduct;
-DROP TABLE IF EXISTS ShoppingCart;
-DROP TABLE IF EXISTS Warehouse;
-DROP TABLE IF EXISTS Favorites;
-DROP TABLE IF EXISTS Employee;
-DROP TABLE IF EXISTS Attachment;
-DROP TABLE IF EXISTS "Comment";
-DROP TABLE IF EXISTS Review;
-DROP TABLE IF EXISTS ProductCost;
-DROP TABLE IF EXISTS ProductCharacteristic;
-DROP TABLE IF EXISTS Characteristic;
-DROP TABLE IF EXISTS ProductCategory;
-DROP TABLE IF EXISTS Category;
-DROP TABLE IF EXISTS Product;
-DROP TABLE IF EXISTS Manufacturer;
-DROP TABLE IF EXISTS ClientAddress;
-DROP TABLE IF EXISTS AuthorizedClient;
-DROP TABLE IF EXISTS "User";
-DROP TABLE IF EXISTS "Role";
+DROP TABLE IF EXISTS supplied_product;
+DROP TABLE IF EXISTS supply;
+DROP TABLE IF EXISTS supplier;
+DROP TABLE IF EXISTS warehouse_product;
+DROP TABLE IF EXISTS order_step;
+DROP TABLE IF EXISTS product_in_order;
+DROP TABLE IF EXISTS "order";
+DROP TABLE IF EXISTS shopping_cart_product;
+DROP TABLE IF EXISTS shopping_cart;
+DROP TABLE IF EXISTS warehouse;
+DROP TABLE IF EXISTS favorites;
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS attachment;
+DROP TABLE IF EXISTS "comment";
+DROP TABLE IF EXISTS review;
+DROP TABLE IF EXISTS product_cost;
+DROP TABLE IF EXISTS product_characteristic;
+DROP TABLE IF EXISTS characteristic;
+DROP TABLE IF EXISTS product_category;
+DROP TABLE IF EXISTS category;
+DROP TABLE IF EXISTS product;
+DROP TABLE IF EXISTS manufacturer;
+DROP TABLE IF EXISTS client_address;
+DROP TABLE IF EXISTS authorized_client;
+DROP TABLE IF EXISTS "user";
+DROP TABLE IF EXISTS "role";
 
-DROP DOMAIN IF EXISTS StatusDomain;
-CREATE DOMAIN StatusDomain AS VARCHAR(10)
+DROP DOMAIN IF EXISTS status_domain;
+CREATE DOMAIN status_domain AS VARCHAR(10)
 CHECK (VALUE IN ('ACTIVE', 'DELETED'));
 
--- Создание таблицы ролей (Role)
-CREATE TABLE "Role" (
-    RoleId SERIAL PRIMARY KEY,
-    RoleName VARCHAR(50) NOT NULL,
-    AccessLevel INT NOT NULL
+-- Создание таблицы ролей (role)
+CREATE TABLE "role" (
+    role_id SERIAL PRIMARY KEY,
+    role_name VARCHAR(50) NOT NULL,
+    access_level INT NOT NULL
 );
 
--- Создание таблицы пользователей (User)
-CREATE TABLE "User" (
-    UserId SERIAL PRIMARY KEY,
-    Login VARCHAR(50) NOT NULL,
-    PassHash VARCHAR(255) NOT NULL,
-    FirstName VARCHAR(50) NOT NULL,
-    SecondName VARCHAR(50),
-    SurName VARCHAR(50),
-    IconLink VARCHAR(255),
-    Phone VARCHAR(20) UNIQUE,
-    Email VARCHAR(100) UNIQUE NOT NULL,
-    BirthdayDate DATE,
-	Status StatusDomain DEFAULT 'ACTIVE',
-    RoleId INT NOT NULL REFERENCES "Role"(RoleId)
+-- Создание таблицы пользователей (user)
+CREATE TABLE "user" (
+    user_id SERIAL PRIMARY KEY,
+    login VARCHAR(50) NOT NULL,
+    pass_hash VARCHAR(255) NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    second_name VARCHAR(50),
+    sur_name VARCHAR(50),
+    icon_link VARCHAR(255),
+    phone VARCHAR(20) UNIQUE,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    birthday_date DATE,
+    status status_domain DEFAULT 'ACTIVE',
+    role_id INT NOT NULL REFERENCES "role"(role_id)
 );
 
 -- Создание таблицы клиентов
-CREATE TABLE AuthorizedClient(
-	UserId SERIAL PRIMARY KEY,
-	Description TEXT,
-	Town VARCHAR(50),
-	Rating NUMERIC(2, 1),
-	FOREIGN KEY (UserId) REFERENCES "User"(UserId)
+CREATE TABLE authorized_client(
+    user_id SERIAL PRIMARY KEY,
+    description TEXT,
+    town VARCHAR(50),
+    rating NUMERIC(2, 1),
+    FOREIGN KEY (user_id) REFERENCES "user"(user_id)
 );
 
 -- Создание таблицы адресов клиентов
-CREATE TABLE ClientAddress (
-    AddressId SERIAL PRIMARY KEY,
-    UserId INT NOT NULL REFERENCES AuthorizedClient,
-    "Name" VARCHAR(100),
-    Town VARCHAR(100),
-    Street VARCHAR(100),
-    Building VARCHAR(20),
-    "Floor" INT,
-    Apartment INT,
-    IntercomCode VARCHAR(20),
-    "Comment" TEXT
+CREATE TABLE client_address (
+    address_id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL REFERENCES authorized_client(user_id),
+    name VARCHAR(100),
+    town VARCHAR(100),
+    street VARCHAR(100),
+    building VARCHAR(20),
+    floor INT,
+    apartment INT,
+    intercom_code VARCHAR(20),
+    comment TEXT
 );
 
--- Создание таблицы производителей (Manufacturer)
-CREATE TABLE Manufacturer (
-    ManufacturerId SERIAL PRIMARY KEY,
-    "Name" VARCHAR(100) NOT NULL,
-    Description TEXT
+-- Создание таблицы производителей (manufacturer)
+CREATE TABLE manufacturer (
+    manufacturer_id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    description TEXT
 );
 
--- Создание таблицы продуктов (Product)
-CREATE TABLE Product (
-    ProductId SERIAL PRIMARY KEY,
-    "Name" VARCHAR(100) NOT NULL,
-    Status StatusDomain NOT NULL DEFAULT 'ACTIVE',
-    Description TEXT,
-    ManufacturerId INT REFERENCES Manufacturer(ManufacturerId)
+-- Создание таблицы продуктов (product)
+CREATE TABLE product (
+    product_id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    status status_domain NOT NULL DEFAULT 'ACTIVE',
+    description TEXT,
+	image_link VARCHAR(255),
+    manufacturer_id INT REFERENCES manufacturer(manufacturer_id)
 );
 
--- Создание таблицы категорий продуктов (Category)
-CREATE TABLE Category (
-    CategoryId SERIAL PRIMARY KEY,
-    "Name" VARCHAR(50) NOT NULL,
-    Description TEXT
+-- Создание таблицы категорий продуктов (category)
+CREATE TABLE category (
+    category_id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    description TEXT
 );
 
--- Создание таблицы связей продукт-категория 
-CREATE TABLE ProductCategory (
-    ProductId INT REFERENCES Product(ProductId) ON DELETE CASCADE,
-    CategoryId INT REFERENCES Category(CategoryId) ON DELETE CASCADE,
-    PRIMARY KEY (ProductId, CategoryId)
+-- Создание таблицы связей продукт-категория
+CREATE TABLE product_category (
+    product_id INT REFERENCES product(product_id) ON DELETE CASCADE,
+    category_id INT REFERENCES category(category_id) ON DELETE CASCADE,
+    PRIMARY KEY (product_id, category_id)
 );
 
--- Создание таблицы характеристик продуктов (Characteristic)
-CREATE TABLE Characteristic (
-    CharacteristicId SERIAL PRIMARY KEY,
-    Name VARCHAR(100) NOT NULL,
-    Category VARCHAR(50)
+-- Создание таблицы характеристик продуктов (characteristic)
+CREATE TABLE characteristic (
+    characteristic_id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    category VARCHAR(50)
 );
 
--- Создание таблицы связей продукт-характеристика (ProductCharacteristic)
-CREATE TABLE ProductCharacteristic (
-    ProductId INT REFERENCES Product(ProductId) ON DELETE CASCADE,
-    CharacteristicId INT REFERENCES Characteristic(CharacteristicId) ON DELETE CASCADE,
-    Value VARCHAR(255),
-    PRIMARY KEY (ProductId, CharacteristicId)
+-- Создание таблицы связей продукт-характеристика (product_characteristic)
+CREATE TABLE product_characteristic (
+    product_id INT REFERENCES product(product_id) ON DELETE CASCADE,
+    characteristic_id INT REFERENCES characteristic(characteristic_id) ON DELETE CASCADE,
+    value VARCHAR(255),
+    PRIMARY KEY (product_id, characteristic_id)
 );
 
--- Создание таблицы цены продукта (ProductCost)
-CREATE TABLE ProductCost (
-    CostId SERIAL PRIMARY KEY,
-    ProductId INT REFERENCES Product(ProductId) ON DELETE CASCADE,
-    Value NUMERIC(10, 2) NOT NULL,
-    Currency VARCHAR(10) NOT NULL,
-    StartTimeStamp TIMESTAMP DEFAULT NOW()
+-- Создание таблицы цены продукта (product_cost)
+CREATE TABLE product_cost (
+    cost_id SERIAL PRIMARY KEY,
+    product_id INT REFERENCES product(product_id) ON DELETE CASCADE,
+    value NUMERIC(10, 2) NOT NULL,
+    currency VARCHAR(10) NOT NULL,
+    start_time_stamp TIMESTAMP DEFAULT NOW()
 );
 
--- Создание таблицы отзывов (Review)
-CREATE TABLE Review (
-    ReviewId SERIAL PRIMARY KEY,
-    UserId INT REFERENCES "User"(UserId),
-    Score INT CHECK (Score >= 1 AND Score <= 5),
-    Description TEXT,
-    TimeStamp TIMESTAMP DEFAULT NOW(),
-    ProductId INT REFERENCES Product(ProductId)
+-- Создание таблицы отзывов (review)
+CREATE TABLE review (
+    review_id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES "user"(user_id),
+    score INT CHECK (score >= 1 AND score <= 5),
+    description TEXT,
+    time_stamp TIMESTAMP DEFAULT NOW(),
+    product_id INT REFERENCES product(product_id)
 );
 
--- Создание таблицы комментариев (Comment)
-CREATE TABLE "Comment" (
-    CommentId SERIAL PRIMARY KEY,
-    UserId INT REFERENCES AuthorizedClient(UserId),
-    ReviewId INT REFERENCES Review(ReviewId),
-    "Text" TEXT,
-    TimeStamp TIMESTAMP DEFAULT NOW()
+-- Создание таблицы комментариев (comment)
+CREATE TABLE "comment" (
+    comment_id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES authorized_client(user_id),
+    review_id INT REFERENCES review(review_id),
+    text TEXT,
+    time_stamp TIMESTAMP DEFAULT NOW()
 );
 
--- Создание таблицы вложений к отзывам (Attachment)
-CREATE TABLE Attachment (
-    Attachment_Link VARCHAR(255) PRIMARY KEY,
-    ReviewId INT REFERENCES Review(ReviewId),
-	Description TEXT
+-- Создание таблицы вложений к отзывам (attachment)
+CREATE TABLE attachment (
+    attachment_link VARCHAR(255) PRIMARY KEY,
+    review_id INT REFERENCES review(review_id),
+    description TEXT
 );
 
-CREATE TABLE Favorites(
-	ProductId INT REFERENCES Product(ProductId) ON DELETE CASCADE,
-    UserId INT REFERENCES AuthorizedClient(UserId) ON DELETE CASCADE,
-    PRIMARY KEY (ProductId, UserId)
+CREATE TABLE favorites(
+    product_id INT REFERENCES product(product_id) ON DELETE CASCADE,
+    user_id INT REFERENCES authorized_client(user_id) ON DELETE CASCADE,
+    PRIMARY KEY (product_id, user_id)
 );
 
--- Создание таблицы корзин покупок (ShoppingCart)
-CREATE TABLE ShoppingCart (
-    CartId SERIAL PRIMARY KEY,
-    ClientId INT REFERENCES AuthorizedClient(UserId)
+-- Создание таблицы корзин покупок (shopping_cart)
+CREATE TABLE shopping_cart (
+    cart_id SERIAL PRIMARY KEY,
+    client_id INT REFERENCES authorized_client(user_id)
 );
 
--- Создание таблицы продуктов в корзине (ShoppingCartProduct)
-CREATE TABLE ShoppingCartProduct (
-    CartId INT REFERENCES ShoppingCart(CartId) ON DELETE CASCADE,
-    ProductId INT REFERENCES Product(ProductId) ON DELETE CASCADE,
-    Count INT NOT NULL,
-    IsSelected BOOLEAN DEFAULT FALSE,
-    PRIMARY KEY (CartId, ProductId)
+-- Создание таблицы продуктов в корзине (shopping_cart_product)
+CREATE TABLE shopping_cart_product (
+    cart_id INT REFERENCES shopping_cart(cart_id) ON DELETE CASCADE,
+    product_id INT REFERENCES product(product_id) ON DELETE CASCADE,
+    count INT NOT NULL,
+    is_selected BOOLEAN DEFAULT FALSE,
+    PRIMARY KEY (cart_id, product_id)
 );
 
--- Создание таблицы заказов (Order)
-CREATE TABLE "Order" (
-    OrderId SERIAL PRIMARY KEY,
-    Status VARCHAR(50) NOT NULL,
-    TimeStamp TIMESTAMP DEFAULT NOW(),
-    CartId INT REFERENCES ShoppingCart(CartId),
-    AddressId INT REFERENCES ClientAddress(AddressId)
+-- Создание таблицы заказов (order)
+CREATE TABLE "order" (
+    order_id SERIAL PRIMARY KEY,
+    status VARCHAR(50) NOT NULL,
+    time_stamp TIMESTAMP DEFAULT NOW(),
+    cart_id INT REFERENCES shopping_cart(cart_id),
+    address_id INT REFERENCES client_address(address_id)
 );
 
--- Создание таблицы сотрудников (Employee)
-CREATE TABLE Employee (
-    EmployeeId SERIAL PRIMARY KEY,
-    Department VARCHAR(100),
-    Vacancy VARCHAR(100),
-    Grade VARCHAR(50),
-    Salary NUMERIC(10, 2),
-	FOREIGN KEY (EmployeeId) REFERENCES "User"(UserId)
+-- Создание таблицы сотрудников (employee)
+CREATE TABLE employee (
+    employee_id SERIAL PRIMARY KEY,
+    department VARCHAR(100),
+    vacancy VARCHAR(100),
+    grade VARCHAR(50),
+    salary NUMERIC(10, 2),
+    FOREIGN KEY (employee_id) REFERENCES "user"(user_id)
 );
 
--- Создание таблицы шагов заказа (OrderStep)
-CREATE TABLE OrderStep (
-    StepId SERIAL PRIMARY KEY,
-    StepName VARCHAR(100) NOT NULL,
-    OrderId INT REFERENCES "Order"(OrderId) ON DELETE CASCADE,
-    TimeStampStart TIMESTAMP,
-    TimeStampEnd TIMESTAMP,
-    Description TEXT,
-    Status VARCHAR(50),
-    ResponsibleId INT REFERENCES Employee(EmployeeId)
+-- Создание таблицы шагов заказа (order_step)
+CREATE TABLE order_step (
+    step_id SERIAL PRIMARY KEY,
+    step_name VARCHAR(100) NOT NULL,
+    order_id INT REFERENCES "order"(order_id) ON DELETE CASCADE,
+    time_stamp_start TIMESTAMP,
+    time_stamp_end TIMESTAMP,
+    description TEXT,
+    status VARCHAR(50),
+    responsible_id INT REFERENCES employee(employee_id)
 );
 
--- Создание таблицы складов (Warehouse)
-CREATE TABLE Warehouse (
-    WarehouseId SERIAL PRIMARY KEY,
-    "Name" VARCHAR(100) NOT NULL,
-    Address TEXT,
-    "Value" NUMERIC(10, 2)
+-- Создание таблицы складов (warehouse)
+CREATE TABLE warehouse (
+    warehouse_id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    address TEXT,
+    value NUMERIC(10, 2)
 );
 
--- Создание таблицы продуктов в корзине (ShoppingCartProduct)
-CREATE TABLE ProductInOrder (
-    OrderId INT REFERENCES "Order"(OrderId) ON DELETE CASCADE,
-    ProductId INT REFERENCES Product(ProductId) ON DELETE CASCADE,
-    "Count" INT NOT NULL,
-    WarehouseId INT NOT NULL REFERENCES Warehouse(WarehouseId),
-    PRIMARY KEY(OrderId, ProductId)
+-- Создание таблицы продуктов на складе (warehouse_product)
+CREATE TABLE warehouse_product (
+    product_id INT REFERENCES product(product_id) ON DELETE CASCADE,
+    warehouse_id INT REFERENCES warehouse(warehouse_id) ON DELETE CASCADE,
+    count INT NOT NULL,
+    section VARCHAR(50),
+    shelf VARCHAR(50),
+    PRIMARY KEY (product_id, warehouse_id)
 );
 
--- Создание таблицы продуктов на складе (WarehouseProduct)
-CREATE TABLE WarehouseProduct (
-    ProductId INT REFERENCES Product(ProductId) ON DELETE CASCADE,
-    WarehouseId INT REFERENCES Warehouse(WarehouseId) ON DELETE CASCADE,
-    "Count" INT NOT NULL,
-    "Section" VARCHAR(50),
-    Shelf VARCHAR(50),
-    PRIMARY KEY (ProductId, WarehouseId)
+-- Создание таблицы поставщиков (supplier)
+CREATE TABLE supplier (
+    supplier_id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    phone VARCHAR(20),
+    address TEXT,
+    contact_name VARCHAR(100),
+    director_name VARCHAR(100)
 );
 
--- Создание таблицы поставщиков (Supplier)
-CREATE TABLE Supplier (
-    SupplierId SERIAL PRIMARY KEY,
-    "Name" VARCHAR(100) NOT NULL,
-    Phone VARCHAR(20),
-    Address TEXT,
-    ContactName VARCHAR(100),
-    DirectorName VARCHAR(100)
+-- Создание таблицы поставок (supply)
+CREATE TABLE supply (
+    supply_id SERIAL PRIMARY KEY,
+    supplier_id INT REFERENCES supplier(supplier_id) ON DELETE CASCADE,
+    time_stamp TIMESTAMP DEFAULT NOW(),
+    count INT NOT NULL,
+    warehouse_id INT REFERENCES warehouse(warehouse_id),
+    responsible_id INT REFERENCES employee(employee_id)
 );
 
--- Создание таблицы поставок (Supply)
-CREATE TABLE Supply (
-    SupplyId SERIAL PRIMARY KEY,
-    SupplierId INT REFERENCES Supplier(SupplierId) ON DELETE CASCADE,
-    TimeStamp TIMESTAMP DEFAULT NOW(),
-    "Count" INT NOT NULL,
-    WarehouseId INT REFERENCES Warehouse(WarehouseId),
-    ResponsibleId INT REFERENCES Employee(EmployeeId)
+-- Создание таблицы продуктов в поставке (supplied_product)
+CREATE TABLE supplied_product (
+    product_id INT REFERENCES product(product_id) ON DELETE CASCADE,
+    supply_id INT REFERENCES supply(supply_id) ON DELETE CASCADE,
+    count INT NOT NULL,
+    price NUMERIC(10, 2),
+    PRIMARY KEY (product_id, supply_id)
 );
 
--- Создание таблицы продуктов в поставке (SuppliedProduct)
-CREATE TABLE SuppliedProduct (
-    ProductId INT REFERENCES Product(ProductId) ON DELETE CASCADE,
-    SupplyId INT REFERENCES Supply(SupplyId) ON DELETE CASCADE,
-    "Count" INT NOT NULL,
-    Price NUMERIC(10, 2),
-    PRIMARY KEY (ProductId, SupplyId)
+-- Создание таблицы продуктов в заказе (product_in_order)
+CREATE TABLE product_in_order (
+    order_id INT REFERENCES "order"(order_id) ON DELETE CASCADE,
+    product_id INT REFERENCES product(product_id) ON DELETE CASCADE,
+    count INT NOT NULL,
+    warehouse_id INT NOT NULL REFERENCES warehouse(warehouse_id),
+    PRIMARY KEY (order_id, product_id)
 );
-
-
-
-
-
-
-
